d21622826493e2f4a01d814bd579c56305f3e0ab
chizakasipascal <pascalkasichiza@gmail.com>
2023-01-22 17:40:25 +0200

    Add double click Scale effect

--- Pinch App/SScreen/ContentView.swift
+++ Pinch App/SScreen/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: -> PROPERTY
-    @State private var isAnimating:Bool=false
-    //MARK: -> FUNCTION
+    @State private var isAnimating:Bool = false
+    @State private var imageScale:CGFloat = 1
+    @State private var imageOffset:CGSize = .zero
     
+    //MARK: -> FUNCTION
+    func restImageState(){
+         return withAnimation(.spring()){
+             imageScale = 1
+             imageOffset = .zero
+         }
+    }
     //MARK: -> CONTENT
     
     var body: some View {
@@ -25,6 +33,34 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12,x: 2,y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x:imageOffset.width,y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    //MARK: -> 1 . TAP GESTURE
+                    .onTapGesture(count: 2,perform:{
+                        if imageScale==1{
+                            withAnimation(.spring()){
+                                imageScale=5
+                            }
+                        }else{
+                            restImageState()
+                        }
+                    })
+                //MARK: -> 2 . DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged{value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset=value.translation
+                                }
+                                
+                            }
+                            .onEnded{_ in
+                                if imageScale <= 1 {
+                                    restImageState()
+                                   
+                                }
+                            }
+                    )
             }// ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
