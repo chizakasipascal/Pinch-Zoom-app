341fa79b2a3b3c1276c0d4a7006d28e380d1526c
chizakasipascal <pascalkasichiza@gmail.com>
2023-01-22 16:57:37 +0200

    implement annimation

--- Pinch App/SScreen/ContentView.swift
+++ Pinch App/SScreen/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: -> PROPERTY
-    
+    @State private var isAnimating:Bool=false
     //MARK: -> FUNCTION
     
     //MARK: -> CONTENT
@@ -19,9 +19,20 @@ struct ContentView: View {
             ZStack{
                 //MARK:  -> IMAGE PAGE
                 Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12,x: 2,y: 2)
+                    .opacity(isAnimating ? 1 : 0)
             }// ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating=true
+                }
+            })
         }// NAVIGATIONVIEW
         .navigationViewStyle(.stack)
     }
