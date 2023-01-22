272787dd30d4268660a5ca183ec9bc9de1146367
chizakasipascal <pascalkasichiza@gmail.com>
2023-01-22 16:04:13 +0200

    Initial Commit

---
+++ Pinch App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch App
+//
+//  Created by CHIZA KASI Pascal on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
